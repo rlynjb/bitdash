60be521062ac17de29532615bce84043dea60f16
rlynjb <root@Reinas-MacBook-Air.local>
2025-02-14 13:10:38 -0800

    add portfolio description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
 export default function Home() {
   return (
-    <div>
-      Welcome to my portfolio!
+    <div className="py-12 px-6 text-sm">
+      <p className="mb-6">
+      With over 6 years of hands-on experience in building complex web-based user interfaces using JavaScript, CSS, and HTML, I have extensive experience with: Vue.js, React.js, Next.js, Nuxt.js, Typescript, Primereact, Tailwind CSS, Daisy UI, and REST APIs.
+      </p>
+      <p className="mb-6">
+I possess a deep understanding of front-end architecture and build processes, including npm, yarn, webpack, and Monorepo setups with Micro-Frontend Architecture, NX, and Turborepo.
+      </p>
+      <p className="mb-6">
+I am also familiar with Unit Testing, Ember.js, Laravel, Rancher, Jenkins, Bash, Vim, Git, Microservices, Containers, WordPress, and Grav (Static Site Generators), and have experimented with GraphQL and Apollo Client/Server.
+      </p>
+
+      <p>
+        I’m currently looking for new opportunities, preferably remote and looking forward to accepting projects.
+        If you have a question or just want to say hi, feel free to contact me via:
+      </p>
+      <ul className="text-center">
+        <li>
+          <a href="https://www.linkedin.com/in/rlynpro/" target="_blank">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://docs.google.com/document/d/1LOO_sdXimhxD43TeWSkoZbqASsT3B68l9YAdkyxmwNU/edit?usp=share_link" target="_blank">
+            Resume
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
