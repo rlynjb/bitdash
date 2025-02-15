233c52fd7ab07125d7c4797e0a710820dcab84e1
rlynjb <root@Reinas-MacBook-Air.local>
2025-02-15 14:32:03 -0800

    organize bst options

--- src/app/trees/binary-search-tree/page.tsx
+++ src/app/trees/binary-search-tree/page.tsx
@@ -15,6 +15,7 @@ export default function BinarySearchTreePage() {
   const [ bstNodes, setBstNodes ] = useState([...defaultValues] as number[]);
 
   const [ insertValue, setInsertValue ] = useState("" as any);
+  const [ deleteValue, setDeleteValue ] = useState("" as any);
   const [ highlightNodes, setHighlightNodes ] = useState([] as any);
 
   const buildDefaultBst = () => {
@@ -30,36 +31,42 @@ export default function BinarySearchTreePage() {
    * modify later when working with highlighting
    * multiple nodes
    */
-  const animateHighlight = async () => {
+  const animateHighlight = async (nodes: any[]) => {
+    setHighlightNodes(nodes);
     //if (highlightNodes.length === 0) return;
     await delayLoop(3000)
     setHighlightNodes([]);
   }
 
 
-  const insert = () => {
+  const insertNode = () => {
     if (insertValue === "") return;
     
     const newVal = parseInt(insertValue);
     setBstNodes((prev) => {
       return [...prev, newVal]
     })
-    setHighlightNodes([newVal]);
     setInsertValue("");
 
-    animateHighlight();
+    animateHighlight([newVal]);
   }
 
   const getMax = () => {
-    setHighlightNodes([bst.max()]);
-
-    animateHighlight();
+    animateHighlight([bst.max()]);
   }
 
   const getMin = () => {
-    setHighlightNodes([bst.min()]);
+    animateHighlight([bst.min()]);
+  }
 
-    animateHighlight();
+  const deleteNode = () => {
+    if (deleteValue === "") return;
+    
+    const val = parseInt(deleteValue);
+    console.log(val)
+
+    // delete in bst
+    // delete in bstNodes
   }
 
 
@@ -74,28 +81,13 @@ export default function BinarySearchTreePage() {
             onChange={e => setInsertValue(e.target.value)}
           />
           <a className="cursor-pointer m-2"
-            onClick={insert}
+            onClick={insertNode}
           >
             Insert
           </a>
         </div>
 
-        <div className="mr-4 py-1">
-          <a className="cursor-pointer m-2"
-            onClick={getMin}
-          >
-            Find Min
-          </a>
-          <a className="cursor-pointer m-2"
-            onClick={getMax}
-          >
-            Find Max
-          </a>
-        </div>
-
-        
-        
-        {/**
+        {/*
         <div className="mr-4">
           <input type="text" className="BInput bg-neutral-800 py-1 px-2 w-16" placeholder="node#" />
           <a className="cursor-pointer m-2"
@@ -103,21 +95,47 @@ export default function BinarySearchTreePage() {
             Search
           </a>
         </div>
+        */}
 
-        <div>
-          <input type="text" className="BInput bg-neutral-800 py-1 px-2 w-16" placeholder="node#" />
+        <div className="mr-4">
+          <input type="text"
+            className="BInput bg-neutral-800 py-1 px-2 w-16"
+            placeholder="node#"
+            value={deleteValue}
+            onChange={e => setDeleteValue(e.target.value)}
+          />
           <a className="cursor-pointer m-2"
+            onClick={deleteNode}
           >
             Delete
           </a>
         </div>
 
-        <div>
-          <a className="cursor-pointer m-2">Find Min</a>
-          <a className="cursor-pointer m-2">Find Max</a>
+        <div className="mr-4 py-1">
+          <span className="text-gray-400 text-xs">Find:</span>
+          <a className="cursor-pointer m-2"
+            onClick={getMin}
+          >
+            Min
+          </a>
+          <a className="cursor-pointer m-2"
+            onClick={getMax}
+          >
+            Max
+          </a>
+          {/*
+            <a className="cursor-pointer m-2">Successor</a>
+            <a className="cursor-pointer m-2">Predecessor</a> 
+          */}
         </div>
-        <a className="cursor-pointer m-2">Find Successor/Predecessor</a>
-        <a className="cursor-pointer m-2">Traversals</a>
+        
+        {/*
+        <div>
+          <b>Traverse:</b>
+          <a className="cursor-pointer m-2">Preorder</a>
+          <a className="cursor-pointer m-2">Inorder</a>
+          <a className="cursor-pointer m-2">Postorder</a>
+        </div>
          */}
       </div>
 
