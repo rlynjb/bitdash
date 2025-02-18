26fb4faa368065b35ebd75f0a1c2188713e6e180
rlynjb <root@Reinas-MacBook-Air.local>
2025-02-18 14:44:59 -0800

    update home page desc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 export default function Home() {
   return (
     <div className="py-12 px-6 text-sm">
-      <h2 className="mb-6">Welcome to my Porfolio & DSA Visualizer...</h2>
+      <h2 className="my-6">Hi! Welcome to my Porfolio & DSA Visualizer...</h2>
       
       <p className="mb-6">
-      Software Engineer - Front End with 7+ years of experience working in small/large teams using Vue.js to develop interactive, user-friendly, and feature-rich web applications. A self-motivated and lifelong learner familiar with modern web development and with a goal to be proficient in DSA and System Design.
+      Software Engineer - Front End here (^_^), with 7+ years of experience working in small/large teams using Vue.js to develop interactive, user-friendly, and feature-rich web applications.
+      </p>
+
+      <p className="mb-6">
+      A self-motivated and lifelong learner familiar with modern web development, with a goal to be proficient in <br/>
+      <b>Data Structures and Algorithm, System Design, and writing code as if I were storytelling! :)</b>
       </p>
 
       <p>
-        I’m currently looking for new opportunities, preferably remote and looking forward to accepting projects in the future.
+        I’m currently looking for new opportunities, preferably remote and looking forward to accepting projects in the future.<br/>
         If you have a question or just want to say hi, feel free to contact me via:
       </p>
       <ul className="text-center">
