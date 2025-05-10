134d7aa3a85bb57b914f47350e6556a8d4f39677
rlynjb <root@Reinas-MacBook-Air.local>
2025-05-09 18:06:23 -0700

    remove export grid visualizer

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,5 @@ export * from "./ArrayVisualizer";
 export * from "./CallstackVisualizer";
 export * from "./LinearDataVisualizer";
 export * from "./BinaryVisualizer";
-export * from "./GridVisualizer";
 export * from "./NetworkDiagram";
 export * from "./ui";
