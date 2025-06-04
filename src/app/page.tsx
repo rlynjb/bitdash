daf018f003498bc8ee6797c6ea68e741f44c55c2
rlynjb <root@Reinas-MacBook-Air.local>
2025-06-04 16:30:56 -0700

    update desc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,25 @@ export default function Home() {
       <h2 className="mb-6">Hi! Welcome to my Porfolio & DSA Visualizer...</h2>
       
       <p className="mb-6">
-      Software Engineer - Front End with 7+ yrs of experience working in small/large teams using Vue.js
+      Software Engineer - Front End with 7+ yrs of experience working in small/large teams
       <br></br>
-      to develop interactive, user-friendly, and feature-rich web applications.
+      using Vue.js to develop interactive, user-friendly, and feature-rich web applications.
       </p>
 
       <p className="mb-6">
-      A self-motivated and lifelong learner familiar with modern web development, with a goal to be proficient in
-      <br></br>
-      <b className="text-red-600">Data Structures & Algorithms and AI tech stack</b>.
+      A self-motivated and lifelong learner familiar with modern web development, <br></br>
+      with a goal to be proficient in <span className="text-red-600">Data Structures & Algorithms and AI tech stack</span>.
       </p>
 
       <p className="mb-6">
         Current tech stack I have experience with are:
         <br></br>
-        <b className="text-red-600">Vue, Nuxt, React, Nextjs, JavaScript, HTML/CSS, and I've dabbled with Python</b>
+        <span className="text-red-600">Vue, Nuxt, React, Nextjs, JavaScript, HTML/CSS, and I've dabbled with Python</span>
+      </p>
+
+      <p className="mb-6">
+        Lately, I also find myself intrigued by <span className="text-red-600">classic Atari games</span>. <br></br>
+        I'm curious how they are built and how DSA are used in those games.
       </p>
 
       <p>
