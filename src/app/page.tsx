c2507057ea3b40edd20aca944378f8e23cbd2f30
rlynjb <root@Reinas-MacBook-Air.local>
2025-06-05 08:21:32 -0700

    update desc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
 export default function Home() {
   return (
-    <div className="p-6 text-sm">
+    <div className="p-6 text-sm text-left sm:max-w-[100%] md:max-w-[80%] lg:max-w-[60%] xl:max-w-[50%]">
       <h2 className="mb-6">Hi! Welcome to my Porfolio & DSA Visualizer...</h2>
       
       <p className="mb-6">
-      Software Engineer - Front End with 7+ yrs of experience working in small/large teams
-      <br></br>
-      using Vue.js to develop interactive, user-friendly, and feature-rich web applications.
+      Software Engineer - Front End with 7+ yrs of experience working in small/large teams using Vue.js to develop interactive, user-friendly, and feature-rich web applications.
       </p>
 
       <p className="mb-6">
-      A self-motivated and lifelong learner familiar with modern web development, <br></br>
-      with a goal to be proficient in <span className="text-red-600">Data Structures & Algorithms</span> and <span className="text-red-600">AI tech stack</span>.
+      A self-motivated and lifelong learner familiar with modern web development, with a goal to be proficient in
+      <br></br>
+      <span className="text-red-600">Data Structures & Algorithms</span> and <span className="text-red-600">AI tech stack</span>.
       </p>
 
       <p className="mb-6">
         Current tech stack I have experience with are:
         <br></br>
-        <span className="text-red-600">Vue, Nuxt, React, Nextjs, JavaScript, HTML/CSS</span>, <br></br>
+        <span className="text-red-600">Vue, Nuxt, React, Nextjs, JavaScript, HTML/CSS</span>,
         and I've dabbled with <span className="text-red-600">Python</span>
       </p>
 
@@ -30,7 +29,7 @@ export default function Home() {
         I look forward to network and see where my skillset takes me.<br/>
         If you have a question or just want to say hi, feel free to contact me via:
       </p>
-      <ul className="text-center">
+      <ul className="text-left mt-4">
         <li>
           <a href="https://www.linkedin.com/in/rlynpro/" target="_blank">
             LinkedIn
