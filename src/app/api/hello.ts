800775eb2d1f88008a07004cf29e9e6e3b9074e5
rlynjb <root@Reinas-Air.domain>
2025-07-06 07:13:36 -0700

    test netlify functions

--- src/app/api/hello.ts
+++ src/app/api/hello.ts
@@ -1,3 +1,4 @@
-export default function handler(req, res) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function handler(req: any, res: any) {
   res.status(200).json({ message: 'Hello, World!' });
 }
